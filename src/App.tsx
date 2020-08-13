9bb30392d1c30f7fa3e80c02c6faab9bb294bd74
Bruno Fernandes <bfbrug@gmail.com>
2020-08-13 17:01:43 -0400

    Validation forms with Yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import SingIn from './pages/SignIn';
+// import SingIn from './pages/SignIn';
+import SingUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SingIn />
+    <SingUp />
     <GlobalStyle />
   </>
 );
